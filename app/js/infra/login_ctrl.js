168a90deef008e58a107c10c50772f71ca4b23b3
alex qian <alexqdjay@126.com>
2014-10-19 21:47:16 +0800

    update

--- app/js/infra/login_ctrl.js
+++ app/js/infra/login_ctrl.js
@@ -10,11 +10,10 @@
 	angular.module('sf.mainApp').controller('LoginCtrl',
 		['$scope','$http',function($scope, $http){
             $http.get('mock/infra/getValid.json',{tmp:Math.random()}).success(function(data) {
-                $scope.isOk = data;
-                if($scope.isOk.result){
-                    $scope.showtext="登陆成功！";
+                if(data.result){
+                    $scope.showtext = "登陆成功！";
                 }else{
-                    $scope.showtext="登陆失败！";
+                    $scope.showtext = "登陆失败！";
                 }
             });
 	}]);
