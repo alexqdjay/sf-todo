8da661615c4d044246b4352c0d308323ca4629eb
alex qian <alexqdjay@126.com>
2014-10-19 11:04:40 +0800

    logup page

---
+++ app/js/infra/logup_ctrl.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * TODO
+ * Created by jolin on 2014/10/19.
+ */
+;
+(function () {
+	'use strict';
+
+	angular.module('sf.mainApp').controller('LogupCtrl',
+		['$scope','$http',function($scope, $http){
+
+	}]);
+
+})();
