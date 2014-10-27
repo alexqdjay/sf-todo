f0dfa41ef374ccb9932289a689e40c7fe3f234a7
wanjiebufu <i563505031@sina.com>
2014-10-27 21:49:41 +0800

    创建&删除项目功能
    创建&删除项目功能

--- app/js/projects/projects_ctrl.js
+++ app/js/projects/projects_ctrl.js
@@ -12,6 +12,21 @@
         $http.get('mock/infra/getProjects1.json',{tmp:Math.random()}).success(function(data) {
             $scope.projects=data;
             $scope.orderProp = 'createtime';
+
+            $scope.addProject = function(){
+              var nowdate = new Date();
+              var nowdate1 = nowdate.toLocaleDateString();
+              var project={
+                  "projname":$scope.name,
+                  "createtime":nowdate1
+              };
+                $scope.projects.push(project);
+                $scope.project ='';
+            }
+
+            $scope.delProject = function($index){
+               $scope.projects.splice($index,1);
+            }
         });
     }]);
 
