de9cd92a40d94471bb0b61e18e65d0fff7e09fa8
wanjiebufu <i563505031@sina.com>
2014-11-09 22:13:14 +0800

    对照代码规范更新代码
    对照代码规范更新代码

--- app/js/projects/projects_ctrl.js
+++ app/js/projects/projects_ctrl.js
@@ -10,31 +10,30 @@
 	angular.module('sf.mainApp').controller('ProjectsCtrl',['$scope','$http',function ProjectsCtrl($scope,$http) {
 
         $http.get('mock/infra/getProjects1.json',{tmp:Math.random()}).success(function(data) {
-            $scope.projects=data;
+            $scope.projects = data;
             $scope.orderProp = 'createtime';
 
-            $scope.addProject = function(){
-              var nowdate = new Date();
-              var nowdate1 = nowdate.toLocaleDateString();
-              var project={
-                  "projname":$scope.name,
-                  "createtime":nowdate1
-              };
+            $scope.addProject = function () {
+                var nowdate = new Date();
+                var nowdate1 = nowdate.toLocaleDateString();
+                var project = {
+                    "projname": $scope.name,
+                    "createtime": nowdate1
+                };
                 $scope.projects.push(project);
-                $scope.project ='';
+                $scope.project = '';
             }
 
-            $scope.delProject = function($index){
-               $scope.projects.splice($index,1);
+            $scope.delProject = function ($index) {
+                $scope.projects.splice($index,1);
             }
-            var dfp='';
-            for(var i = 0; i <$scope.projects.length; i++)
-            {
-                if($scope.projects[i].default == true){
-                    dfp=$scope.projects[i];
+            var dfp = '';
+            for (var i = 0; i < $scope.projects.length; i++) {
+                if ($scope.projects[i].default == true){
+                    dfp = $scope.projects[i];
                 }
             }
-            $scope.defaultProj =dfp;
+            $scope.defaultProj = dfp;
         });
     }]);
 
